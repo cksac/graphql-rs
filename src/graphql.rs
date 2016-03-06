c2b9815b2455b0a726e8af710e83a9eeb743e56b
cksac <cs.cksac@gmail.com>
2016-03-06 23:02:12 +0800

    add GraphQLOject

--- src/graphql.rs
+++ src/graphql.rs
@@ -1,6 +1,8 @@
 use std::collections::HashMap;
 use std::str::FromStr;
 use std::any::Any;
+use std::cell::RefCell;
+use std::rc::Rc;
 
 macro_rules! blanket_impl {
   ($trait_: ident for $($type_: ty),*) => {
@@ -48,7 +50,7 @@ pub trait GraphQLType {
   fn name(&self) -> &str;
   fn description(&self) -> Option<&str>;
 }
-impl_graphql_type_for! { GraphQLEnum }
+impl_graphql_type_for! { GraphQLEnum, GraphQLObject }
 
 pub trait GraphQLScalar: GraphQLType {
   type ValueType: Any;
@@ -85,48 +87,243 @@ pub struct GraphQLEnum {
   values: HashMap<String, GraphQLEnumValue>,
 }
 
-impl GraphQLEnum {
-  fn values(&self) -> &HashMap<String, GraphQLEnumValue> {
-    &self.values
-  }
-}
-
 pub struct GraphQLEnumValue {
   value: String,
   description: Option<String>,
   depreciation_reason: Option<String>,
 }
 
-impl GraphQLEnumValue {
-  pub fn new(value: &str) -> GraphQLEnumValue {
-    GraphQLEnumValue {
-      value: value.to_owned(),
+
+pub trait GraphQLOutput: GraphQLType {}
+blanket_impl! { GraphQLOutput for GraphQLInt, GraphQLFloat, GraphQLString, GraphQLBoolean, GraphQLObject }
+impl<T> GraphQLOutput for GraphQLScalar<ValueType = T> {}
+
+pub trait GraphQLInput: GraphQLType {}
+blanket_impl! { GraphQLInput for GraphQLInt, GraphQLFloat, GraphQLString, GraphQLBoolean }
+impl<T> GraphQLInput for GraphQLScalar<ValueType = T> {}
+
+pub struct GraphQLObject {
+  name: String,
+  description: Option<String>,
+  fields: RefCell<HashMap<String, GraphQLField>>,
+}
+
+impl GraphQLObject {
+  pub fn replace_field_placeholder_type<T: GraphQLOutput + 'static>(&self,
+                                                                    field_name: &str,
+                                                                    other_type: &Rc<T>) {
+    let mut field = self.fields.borrow_mut().remove(field_name);
+    if field.is_none() {
+      panic!("Object type {:} does not have placeholder {:} field.",
+             self.name,
+             field_name);
+    }
+
+    if let Some(mut f) = field {
+      let f_type_name = f.typ.name().to_owned();
+      if !f_type_name.ends_with("___TypePlaceholder___") {
+        panic!("Field {:} in object type {:} is not a placeholder.",
+               field_name,
+               self.name);
+      }
+
+      let target_type = f_type_name.trim_right_matches("___TypePlaceholder___");
+      if target_type != other_type.name() {
+        panic!("Placeholder {:} in object type {:} should replaced by {:} type instead of \
+                {:} type.",
+               field_name,
+               self.name,
+               target_type,
+               other_type.name());
+      }
+
+      f.typ = other_type.clone();
+      self.fields.borrow_mut().insert(field_name.to_owned(), f);
+    }
+  }
+}
+
+pub struct GraphQLField {
+  name: String,
+  description: Option<String>,
+  depreciation_reason: Option<String>,
+  typ: Rc<GraphQLOutput>,
+  args: Option<HashMap<String, GraphQLArgument>>,
+}
+
+pub struct GraphQLArgument {
+  name: String,
+  description: Option<String>,
+  typ: Rc<GraphQLInput>,
+}
+
+
+// Builders
+struct Placeholder {
+  name: String,
+}
+
+impl Placeholder {
+  fn new(target_type_name: &str) -> Placeholder {
+    Placeholder { name: format!("{:}___TypePlaceholder___", target_type_name) }
+  }
+}
+
+impl GraphQLType for Placeholder {
+  fn name(&self) -> &str {
+    self.name.as_ref()
+  }
+
+  fn description(&self) -> Option<&str> {
+    None
+  }
+}
+
+blanket_impl! { GraphQLOutput for Placeholder }
+
+pub struct GraphQLObjectBuilder {
+  name: String,
+  description: Option<String>,
+  fields: HashMap<String, GraphQLField>,
+}
+
+impl GraphQLObjectBuilder {
+  pub fn new(name: &str) -> GraphQLObjectBuilder {
+    GraphQLObjectBuilder {
+      name: name.to_owned(),
       description: None,
-      depreciation_reason: None,
+      fields: HashMap::new(),
     }
   }
 
-  pub fn value(&self) -> &str {
-    self.value.as_ref()
-  }
-
-  pub fn description(&self) -> Option<&str> {
-    self.description.as_ref().map(|s| s.as_ref())
-  }
-
-  pub fn with_description(mut self, description: &str) -> GraphQLEnumValue {
+  pub fn with_description(mut self, description: &str) -> GraphQLObjectBuilder {
     self.description = Some(description.to_owned());
     self
   }
 
-  pub fn mark_depreciated(mut self, depreciation_reason: &str) -> GraphQLEnumValue {
-    let reason = depreciation_reason.trim().to_owned();
-    if reason.len() == 0 {
-      panic!("Deprecation reason for enum value {:?} cannot be empty",
-             self.value);
-    }
-    self.depreciation_reason = Some(reason);
+  pub fn field<F>(mut self, name: &str, f: F) -> GraphQLObjectBuilder
+    where F: Fn(GraphQLFieldBuilder) -> GraphQLFieldBuilder
+  {
+    let field = f(GraphQLFieldBuilder::new(name)).build();
+    self.fields.insert(name.to_owned(), field);
     self
+  }
+
+  pub fn build(self) -> Rc<GraphQLObject> {
+    if self.fields.len() == 0 {
+      panic!("Object type {:} must contains at least one field",
+             self.name);
+    }
+
+    Rc::new(GraphQLObject {
+      name: self.name,
+      description: self.description,
+      fields: RefCell::new(self.fields),
+    })
+  }
+}
+
+pub struct GraphQLFieldBuilder {
+  name: String,
+  description: Option<String>,
+  depreciation_reason: Option<String>,
+  typ: Option<Rc<GraphQLOutput>>,
+  args: Option<HashMap<String, GraphQLArgument>>,
+}
+
+impl GraphQLFieldBuilder {
+  fn new(name: &str) -> GraphQLFieldBuilder {
+    GraphQLFieldBuilder {
+      name: name.to_owned(),
+      description: None,
+      depreciation_reason: None,
+      typ: None,
+      args: None,
+    }
+  }
+
+  pub fn with_description(mut self, description: &str) -> GraphQLFieldBuilder {
+    self.description = Some(description.to_owned());
+    self
+  }
+
+  pub fn mark_depreciated(mut self, reason: &str) -> GraphQLFieldBuilder {
+    self.depreciation_reason = Some(reason.to_owned());
+    self
+  }
+
+  pub fn type_of<T: GraphQLOutput + 'static>(mut self, typ: &Rc<T>) -> GraphQLFieldBuilder {
+    self.typ = Some(typ.clone());
+    self
+  }
+
+  pub fn placeholder_type_of(mut self, target_type: &str) -> GraphQLFieldBuilder {
+    self.typ = Some(Rc::new(Placeholder::new(target_type)));
+    self
+  }
+
+  pub fn arg<F>(mut self, name: &str, f: F) -> GraphQLFieldBuilder
+    where F: Fn(GraphQLArgumentBuilder) -> GraphQLArgumentBuilder
+  {
+    let arg = f(GraphQLArgumentBuilder::new(name)).build();
+    match self.args {
+      Some(ref mut args) => {
+        args.insert(name.to_owned(), arg);
+      }
+      None => {
+        let mut args = HashMap::new();
+        args.insert(name.to_owned(), arg);
+        self.args = Some(args);
+      }
+    }
+    self
+  }
+
+  pub fn build(self) -> GraphQLField {
+    if self.typ.is_none() {
+      panic!("Field {:} missing type defination", self.name);
+    }
+
+    GraphQLField {
+      name: self.name,
+      description: self.description,
+      depreciation_reason: self.depreciation_reason,
+      typ: self.typ.unwrap(),
+      args: None,
+    }
+  }
+}
+
+pub struct GraphQLArgumentBuilder {
+  name: String,
+  description: Option<String>,
+  typ: Option<Rc<GraphQLInput>>,
+}
+
+impl GraphQLArgumentBuilder {
+  pub fn new(name: &str) -> GraphQLArgumentBuilder {
+    GraphQLArgumentBuilder {
+      name: name.to_owned(),
+      description: None,
+      typ: None,
+    }
+  }
+
+  pub fn type_of<T: GraphQLInput + 'static>(mut self, typ: &Rc<T>) -> GraphQLArgumentBuilder {
+    self.typ = Some(typ.clone());
+    self
+  }
+
+  pub fn build(self) -> GraphQLArgument {
+    if self.typ.is_none() {
+      panic!("Argument {:} missing type defination", self.name);
+    }
+
+    GraphQLArgument {
+      name: self.name,
+      description: self.description,
+      typ: self.typ.unwrap(),
+    }
   }
 }
 
@@ -137,7 +334,7 @@ pub struct GraphQLEnumBuilder {
 }
 
 impl GraphQLEnumBuilder {
-  fn new(name: &str) -> GraphQLEnumBuilder {
+  pub fn new(name: &str) -> GraphQLEnumBuilder {
     GraphQLEnumBuilder {
       name: name.to_owned(),
       description: None,
@@ -145,27 +342,22 @@ impl GraphQLEnumBuilder {
     }
   }
 
-  fn with_description(mut self, description: &str) -> GraphQLEnumBuilder {
+  pub fn with_description(mut self, description: &str) -> GraphQLEnumBuilder {
     self.description = Some(description.to_owned());
     self
   }
 
-  fn value_fn<F>(mut self, name: &str, f: F) -> GraphQLEnumBuilder
-    where F: FnOnce(GraphQLEnumValue) -> GraphQLEnumValue
+  pub fn value<F>(mut self, name: &str, f: F) -> GraphQLEnumBuilder
+    where F: Fn(GraphQLEnumValueBuilder) -> GraphQLEnumValueBuilder
   {
-    let v = f(GraphQLEnumValue::new(name));
-    self.values.insert(v.value.clone(), v);
+    let v = f(GraphQLEnumValueBuilder::new(name)).build();
+    self.values.insert(name.to_owned(), v);
     self
   }
 
-  fn value(mut self, name: &str) -> GraphQLEnumBuilder {
-    self.values.insert(name.to_owned(), GraphQLEnumValue::new(name));
-    self
-  }
-
-  fn build(self) -> GraphQLEnum {
+  pub fn build(self) -> GraphQLEnum {
     if self.values.len() == 0 {
-      panic!("Enum {:?} must has at least one value defined.", self.name);
+      panic!("Enum {:} must has at least one value defined.", self.name);
     }
 
     GraphQLEnum {
@@ -176,9 +368,50 @@ impl GraphQLEnumBuilder {
   }
 }
 
+pub struct GraphQLEnumValueBuilder {
+  value: String,
+  description: Option<String>,
+  depreciation_reason: Option<String>,
+}
+
+impl GraphQLEnumValueBuilder {
+  pub fn new(value: &str) -> GraphQLEnumValueBuilder {
+    GraphQLEnumValueBuilder {
+      value: value.to_owned(),
+      description: None,
+      depreciation_reason: None,
+    }
+  }
+
+  pub fn with_description(mut self, description: &str) -> GraphQLEnumValueBuilder {
+    self.description = Some(description.to_owned());
+    self
+  }
+
+  pub fn mark_depreciated(mut self, depreciation_reason: &str) -> GraphQLEnumValueBuilder {
+    let reason = depreciation_reason.trim().to_owned();
+    if reason.len() == 0 {
+      panic!("Deprecation reason for enum value {:} cannot be empty",
+             self.value);
+    }
+    self.depreciation_reason = Some(reason);
+    self
+  }
+
+  pub fn build(self) -> GraphQLEnumValue {
+    GraphQLEnumValue {
+      value: self.value,
+      description: self.description,
+      depreciation_reason: self.depreciation_reason,
+    }
+  }
+}
+
 #[cfg(test)]
 mod tests {
   use super::*;
+  use std::rc::Rc;
+  use std::cell::RefCell;
 
   #[test]
   fn test_scalar_type() {
@@ -216,23 +449,64 @@ mod tests {
   #[test]
   fn test_enum_type() {
     let rgb = GraphQLEnumBuilder::new("RGB")
-                .value("RED")
-                .value("GREEN")
-                .value("BLUE")
+                .value("RED", |v| v)
+                .value("GREEN", |v| v)
+                .value("BLUE", |v| v)
                 .build();
-    assert_eq!(3, rgb.values().len());
+    assert_eq!(3, rgb.values.len());
 
     let days = GraphQLEnumBuilder::new("DAYS")
                  .with_description("Days of the week")
-                 .value_fn("SAT", |v| v.with_description("Satarday"))
-                 .value_fn("SUN", |v| v.with_description("Sunday"))
-                 .value_fn("MON", |v| v.with_description("Monday"))
-                 .value_fn("TUE", |v| v.with_description("Tuesday"))
-                 .value_fn("WED", |v| v.with_description("Wedsday"))
-                 .value_fn("THU", |v| v.with_description("Thusday"))
-                 .value_fn("FRI", |v| v.with_description("Friday"))
+                 .value("SAT", |v| v.with_description("Satarday"))
+                 .value("SUN", |v| v.with_description("Sunday"))
+                 .value("MON", |v| v.with_description("Monday"))
+                 .value("TUE", |v| v.with_description("Tuesday"))
+                 .value("WED", |v| v.with_description("Wedsday"))
+                 .value("THU", |v| v.with_description("Thusday"))
+                 .value("FRI", |v| v.with_description("Friday"))
                  .build();
-    assert_eq!("Days of the week", days.description().unwrap_or(""));
-    assert_eq!("Monday", days.values()["MON"].description().unwrap_or(""));;
+    assert_eq!(Some("Days of the week"),
+               days.description.as_ref().map(|s| s.as_ref()));
+    assert_eq!(Some("Monday"),
+               days.values["MON"].description.as_ref().map(|s| s.as_ref()));
+  }
+
+  #[test]
+  fn test_object_type() {
+    let INT = &Rc::new(GraphQLInt);
+    let FLOAT = &Rc::new(GraphQLFloat);
+    let STRING = &Rc::new(GraphQLString);
+    let BOOLEAN = &Rc::new(GraphQLBoolean);
+
+    let IMAGE = &GraphQLObjectBuilder::new("Image")
+                   .with_description("Image Type")
+                   .field("url", |f| f.type_of(STRING))
+                   .field("width", |f| f.type_of(INT))
+                   .field("height", |f| f.type_of(INT))
+                   .build();
+
+    let AUTHOR = &GraphQLObjectBuilder::new("Author")
+                    .with_description("Author Type")
+                    .field("id", |f| f.type_of(STRING))
+                    .field("name", |f| f.type_of(STRING))
+                    .field("pic", |f| {
+                      f.type_of(IMAGE)
+                       .arg("width", |a| a.type_of(INT))
+                       .arg("height", |a| a.type_of(INT))
+                    })
+                    .field("recentArticle", |f| f.placeholder_type_of("Article"))
+                    .build();
+
+    let ARTICLE = &GraphQLObjectBuilder::new("Article")
+                     .field("id", |f| f.type_of(STRING))
+                     .field("isPublished", |f| f.type_of(BOOLEAN))
+                     .field("author", |f| f.type_of(AUTHOR))
+                     .field("title", |f| f.type_of(STRING))
+                     .field("body", |f| f.type_of(STRING))
+                     .build();
+
+    AUTHOR.replace_field_placeholder_type("recentArticle", ARTICLE);
+    assert_eq!("Article",
+               AUTHOR.fields.borrow()["recentArticle"].typ.name());
   }
 }
