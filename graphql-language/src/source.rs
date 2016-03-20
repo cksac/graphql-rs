9a2786460bc8843990ba4e21a910a96424ca9f46
Victor M. Suarez <svmnotn@gmail.com>
2016-03-20 17:52:52 -0400

    feat: add from_file

--- graphql-language/src/source.rs
+++ graphql-language/src/source.rs
@@ -1,23 +1,46 @@
+use std::io;
+use std::io::prelude::*;
+use std::fs::File;
+use std::path::{Path, PathBuf};
+
 static DEFAULT_SOURCE_NAME: &'static str = "GraphQL";
 
 #[derive(Debug)]
-pub struct Source<'a> {
-  pub name: &'a str,
-  pub body: &'a str,
+pub struct Source {
+  pub name: String,
+  pub body: String,
 }
 
-impl<'a> Source<'a> {
-  pub fn new(body: &'a str) -> Source<'a> {
+impl Source {
+  pub fn new(body: String) -> Self {
     Source {
-      name: DEFAULT_SOURCE_NAME,
+      name: DEFAULT_SOURCE_NAME.to_owned(),
       body: body,
     }
   }
 
-  pub fn with_name(name: &'a str, body: &'a str) -> Source<'a> {
+  pub fn with_name(name: String, body: String) -> Self {
     Source {
       name: name,
       body: body,
     }
+  }
+
+  pub fn from_file<P: AsRef<Path>>(path: P) -> Result<Self, io::Error> {
+    let mut p = PathBuf::new();
+    p.push(path);
+    let mut f = try!(File::open(p.clone()));
+    let mut s = String::new();
+    try!(f.read_to_string(&mut s));
+    Ok(Source::with_name(Self::get_file_name(p), s))
+  }
+
+  fn get_file_name(path: PathBuf) -> String {
+    if let Some(n) = path.as_path().file_name() {
+      if let Ok(name) = n.to_os_string().into_string() {
+        return name;
+      }
+    }
+    DEFAULT_SOURCE_NAME.to_owned()
   }
 }
