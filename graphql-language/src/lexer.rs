c6c4048c2cf958c83712cf9913ad0039432abda4
Victor M. Suarez <svmnotn@gmail.com>
2016-03-20 11:58:20 -0400

    feat: use new Punctuator enum, and fix tests

--- graphql-language/src/lexer.rs
+++ graphql-language/src/lexer.rs
@@ -190,12 +190,12 @@ fn scan_string(lexer: &mut Lexer) -> (bool, bool) {
   (is_terminated, is_bad_escape)
 }
 
-// Int:   (+|-)?(0|[1-9][0-9]*)
-// Float: (+|-)?(0|[1-9][0-9]*)(\.[0-9]+)?((E|e)(+|-)?[0-9]+)?
+// IntValue:   (+|-)?(0|[1-9][0-9]*)
+// FloatValue: (+|-)?(0|[1-9][0-9]*)(\.[0-9]+)?((E|e)(+|-)?[0-9]+)?
 fn scan_number(lexer: &mut Lexer) -> (bool, bool) {
   let mut is_float = false;
   take!(lexer, '+' | '-');
-  // Integer part
+  // IntValueeger part
   if take!(lexer, '1'...'9') {
     take_while!(lexer, '0'...'9');
     if take!(lexer, ' ' | '\t' | '\r' | '\n' | ',') || take_eof!(lexer) {
@@ -237,6 +237,9 @@ fn scan_number(lexer: &mut Lexer) -> (bool, bool) {
 impl<'a> Iterator for Lexer<'a> {
   type Item = Result<Token<'a>, &'a str>;
   fn next(&mut self) -> Option<Result<Token<'a>, &'a str>> {
+    use self::Token::*;
+    use self::Punctuator::*;
+
     if !self.is_eof && take_eof!(self) {
       self.is_eof = true;
       return Some(Ok(Token::Eof(self.lo, self.hi)));
@@ -253,76 +256,76 @@ impl<'a> Iterator for Lexer<'a> {
       item = match c {
         '!' => {
           self.iter.next();
-          Some(Ok(Token::Bang(self.lo, self.hi)))
+          Some(Ok(Punctuator(Bang, self.lo)))
         }
         '$' => {
           self.iter.next();
-          Some(Ok(Token::Dollar(self.lo, self.hi)))
+          Some(Ok(Punctuator(Dollar, self.lo)))
         }
         '(' => {
           self.iter.next();
-          Some(Ok(Token::LeftParen(self.lo, self.hi)))
+          Some(Ok(Punctuator(LeftParen, self.lo)))
         }
         ')' => {
           self.iter.next();
-          Some(Ok(Token::RightParen(self.lo, self.hi)))
+          Some(Ok(Punctuator(RightParen, self.lo)))
         }
         ':' => {
           self.iter.next();
-          Some(Ok(Token::Colon(self.lo, self.hi)))
+          Some(Ok(Punctuator(Colon, self.lo)))
         }
         '=' => {
           self.iter.next();
-          Some(Ok(Token::Equals(self.lo, self.hi)))
+          Some(Ok(Punctuator(Equals, self.lo)))
         }
         '@' => {
           self.iter.next();
-          Some(Ok(Token::At(self.lo, self.hi)))
+          Some(Ok(Punctuator(At, self.lo)))
         }
         '[' => {
           self.iter.next();
-          Some(Ok(Token::LeftBracket(self.lo, self.hi)))
+          Some(Ok(Punctuator(LeftBracket, self.lo)))
         }
         ']' => {
           self.iter.next();
-          Some(Ok(Token::RightBracket(self.lo, self.hi)))
+          Some(Ok(Punctuator(RightBracket, self.lo)))
         }
         '{' => {
           self.iter.next();
-          Some(Ok(Token::LeftBrace(self.lo, self.hi)))
+          Some(Ok(Punctuator(LeftBrace, self.lo)))
         }
         '}' => {
           self.iter.next();
-          Some(Ok(Token::RightBrace(self.lo, self.hi)))
+          Some(Ok(Punctuator(RightBrace, self.lo)))
         }
         '|' => {
           self.iter.next();
-          Some(Ok(Token::Pipe(self.lo, self.hi)))
+          Some(Ok(Punctuator(Pipe, self.lo)))
         }
         '.' => {
           if scan_spread(self) {
-            Some(Ok(Token::Spread(self.lo, self.hi)))
+            Some(Ok(Punctuator(Spread(self.lo), self.hi)))
           } else {
             Some(Err("Unexpected character."))
           }
         }
         '_' | 'a'...'z' | 'A'...'Z' => {
           scan_name(self);
-          Some(Ok(Token::Name(self.lo, self.hi, &self.input[self.lo..self.hi])))
+          Some(Ok(Name(&self.input[self.lo..self.hi], self.lo, self.hi)))
         }
         '-' | '0'...'9' => {
           match scan_number(self) {
             (false, false) => Some(Err("Invalid integer number.")),
             (false, true) => Some(Err("Invalid float number.")),
-            (true, false) => Some(Ok(Token::Int(self.lo, self.hi, &self.input[self.lo..self.hi]))),
-            (true, true) => Some(Ok(Token::Float(self.lo, self.hi, &self.input[self.lo..self.hi]))),
+            (true, false) => Some(Ok(IntValue(&self.input[self.lo..self.hi], self.lo, self.hi))),
+            (true, true) => Some(Ok(FloatValue(&self.input[self.lo..self.hi], self.lo, self.hi))),
           }
         }
         '"' => {
           match scan_string(self) {
             (false, _) => Some(Err("Unterminated string.")),
             (_, true) => Some(Err("Bad character escape sequence.")),
-            _ => Some(Ok(Token::String(self.lo, self.hi, &self.input[self.lo..self.hi]))),
+            _ => Some(Ok(StringValue(&self.input[self.lo..self.hi], self.lo, self.hi))),
           }
         }
         _ => Some(Err("Unexpected character.")),
@@ -334,7 +337,9 @@ impl<'a> Iterator for Lexer<'a> {
 
 #[cfg(test)]
 mod tests {
-  use super::*;
+  use super::Punctuator::*;
+  use super::Token::*;
+  use super::{Lexer, Token};
 
   fn test_next_token(lexer: &mut Lexer, expected: Token) {
     let next = lexer.next();
@@ -348,7 +353,7 @@ mod tests {
 
   fn test_str(input: &str, expeced: &str) {
     let len = expeced.len();
-    test_with_lex(input, Token::String(1, len + 1, expeced))
+    test_with_lex(input, StringValue(expeced, 1, len + 1))
   }
 
   #[test]
@@ -364,12 +369,12 @@ mod tests {
   #[test]
   fn test_name() {
     let mut lexer = Lexer::new("     name1    name2   \"simple\" \"  white space   \" other_name");
-    test_next_token(&mut lexer, Token::Name(5, 10,"name1"));
-    test_next_token(&mut lexer, Token::Name(14,19,"name2"));
-    test_next_token(&mut lexer, Token::String(23,29,"simple"));
-    test_next_token(&mut lexer, Token::String(32,48,"  white space   "));
-    test_next_token(&mut lexer, Token::Name(50,60,"other_name"));
-    test_next_token(&mut lexer, Token::Eof(50,60));
+    test_next_token(&mut lexer, Name("name1", 5, 10));
+    test_next_token(&mut lexer, Name("name2", 14, 19));
+    test_next_token(&mut lexer, StringValue("simple", 23, 29));
+    test_next_token(&mut lexer, StringValue("  white space   ", 32, 48));
+    test_next_token(&mut lexer, Name("other_name", 50, 60));
+    test_next_token(&mut lexer, Eof(50,60));
     assert_eq!(None, lexer.next());
   }
 
@@ -377,44 +382,44 @@ mod tests {
   fn test_number() {
     let mut lexer = Lexer::new("-9 0 9 -0 -9 1234 -1234 0.0 -0.0 1.0 -1.012 -0.101 1.0e2 -1.0E2 \
                                 0.00e-002 -0.00E+002 9.1E+0");
-    test_next_token(&mut lexer, Token::Int(0,2,    "-9"));
-    test_next_token(&mut lexer, Token::Int(3,4,    "0"));
-    test_next_token(&mut lexer, Token::Int(5,6,    "9"));
-    test_next_token(&mut lexer, Token::Int(7,9,    "-0"));
-    test_next_token(&mut lexer, Token::Int(10,12,  "-9"));
-    test_next_token(&mut lexer, Token::Int(13,17,  "1234"));
-    test_next_token(&mut lexer, Token::Int(18,23,  "-1234"));
-    test_next_token(&mut lexer, Token::Float(24,27,"0.0"));
-    test_next_token(&mut lexer, Token::Float(28,32,"-0.0"));
-    test_next_token(&mut lexer, Token::Float(33,36,"1.0"));
-    test_next_token(&mut lexer, Token::Float(37,43,"-1.012"));
-    test_next_token(&mut lexer, Token::Float(44,50,"-0.101"));
-    test_next_token(&mut lexer, Token::Float(51,56,"1.0e2"));
-    test_next_token(&mut lexer, Token::Float(57,63,"-1.0E2"));
-    test_next_token(&mut lexer, Token::Float(64,73,"0.00e-002"));
-    test_next_token(&mut lexer, Token::Float(74,84,"-0.00E+002"));
-    test_next_token(&mut lexer, Token::Float(85,91,"9.1E+0"));
-    test_next_token(&mut lexer, Token::Eof(85,91));
+    test_next_token(&mut lexer, IntValue("-9", 0, 2));
+    test_next_token(&mut lexer, IntValue("0", 3, 4));
+    test_next_token(&mut lexer, IntValue("9", 5, 6));
+    test_next_token(&mut lexer, IntValue("-0", 7, 9));
+    test_next_token(&mut lexer, IntValue("-9", 10, 12));
+    test_next_token(&mut lexer, IntValue("1234", 13, 17));
+    test_next_token(&mut lexer, IntValue("-1234", 18, 23));
+    test_next_token(&mut lexer, FloatValue("0.0", 24, 27));
+    test_next_token(&mut lexer, FloatValue("-0.0", 28, 32));
+    test_next_token(&mut lexer, FloatValue("1.0", 33, 36));
+    test_next_token(&mut lexer, FloatValue("-1.012", 37, 43));
+    test_next_token(&mut lexer, FloatValue("-0.101", 44, 50));
+    test_next_token(&mut lexer, FloatValue("1.0e2", 51, 56));
+    test_next_token(&mut lexer, FloatValue("-1.0E2", 57, 63));
+    test_next_token(&mut lexer, FloatValue("0.00e-002", 64, 73));
+    test_next_token(&mut lexer, FloatValue("-0.00E+002", 74, 84));
+    test_next_token(&mut lexer, FloatValue("9.1E+0", 85, 91));
+    test_next_token(&mut lexer, Eof(85,91));
     assert_eq!(None, lexer.next());
   }
 
   #[test]
   fn test_symbol() {
     let mut lexer = Lexer::new("@ !  :$ =    [  {   (|] } )      ...");
-    test_next_token(&mut lexer, Token::At(0,0));
-    test_next_token(&mut lexer, Token::Bang(2,2));
-    test_next_token(&mut lexer, Token::Colon(5,5));
-    test_next_token(&mut lexer, Token::Dollar(6,6));
-    test_next_token(&mut lexer, Token::Equals(8,8));
-    test_next_token(&mut lexer, Token::LeftBracket(13,13));
-    test_next_token(&mut lexer, Token::LeftBrace(16,16));
-    test_next_token(&mut lexer, Token::LeftParen(20,20));
-    test_next_token(&mut lexer, Token::Pipe(21,21));
-    test_next_token(&mut lexer, Token::RightBracket(22,22));
-    test_next_token(&mut lexer, Token::RightBrace(24,24));
-    test_next_token(&mut lexer, Token::RightParen(26,26));
-    test_next_token(&mut lexer, Token::Spread(33,35));
-    test_next_token(&mut lexer, Token::Eof(33,36));
+    test_next_token(&mut lexer, Punctuator(At, 0));
+    test_next_token(&mut lexer, Punctuator(Bang, 2));
+    test_next_token(&mut lexer, Punctuator(Colon, 5));
+    test_next_token(&mut lexer, Punctuator(Dollar, 6));
+    test_next_token(&mut lexer, Punctuator(Equals, 8));
+    test_next_token(&mut lexer, Punctuator(LeftBracket, 13));
+    test_next_token(&mut lexer, Punctuator(LeftBrace, 16));
+    test_next_token(&mut lexer, Punctuator(LeftParen, 20));
+    test_next_token(&mut lexer, Punctuator(Pipe, 21));
+    test_next_token(&mut lexer, Punctuator(RightBracket, 22));
+    test_next_token(&mut lexer, Punctuator(RightBrace, 24));
+    test_next_token(&mut lexer, Punctuator(RightParen, 26));
+    test_next_token(&mut lexer, Punctuator(Spread(33), 35));
+    test_next_token(&mut lexer, Eof(33,36));
     assert_eq!(None, lexer.next());
   }
 }
