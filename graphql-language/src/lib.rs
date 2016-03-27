104c679498213037b3afc33e2a10bb3a08e991b1
Victor M. Suarez <svmnotn@gmail.com>
2016-03-26 21:20:30 -0400

    add parser entry point

--- graphql-language/src/lib.rs
+++ graphql-language/src/lib.rs
@@ -1,3 +1,32 @@
 pub mod ast;
-pub mod lexer;
-pub mod source;
+mod source;
+mod lexer;
+mod parser;
+
+use ast::Document;
+use source::{Source, from_file};
+
+pub fn parse<'a>(input: &'a str) -> Document<'a> {
+  parse_source(Source::new(input))
+}
+
+use std::path::Path;
+
+pub fn parse_file<'a>(file: &'a Path, buf: &'a mut String) -> Document<'a> {
+  parse_source(from_file(file, buf).unwrap()) // FIXME
+}
+
+fn parse_source<'a>(src: Source<'a>) -> Document<'a> {
+  let mut res = Document {
+    source: src,
+    definitions: Vec::new(),
+  };
+
+  let parser = parser::Parser::new(res.source);
+
+  for def in parser {
+    res.definitions.push(def.unwrap());// FIXME
+  }
+
+  res
+}
