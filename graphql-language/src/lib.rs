31667b8de1106d0d434e6b699f318cb7628aba91
Victor M. Suarez <svmnotn@gmail.com>
2016-06-24 22:57:37 -0400

    use Root in main helper methods

--- graphql-language/src/lib.rs
+++ graphql-language/src/lib.rs
@@ -1,26 +1,16 @@
-#![allow(unused_variables)]
+#![allow(unused_variables, dead_code, unused_mut, unused_imports)]
 pub mod ast;
 mod error;
-mod source;
 mod lexer;
 mod parser;
 
 pub use error::Error;
 pub type Result<T> = std::result::Result<T, Error>;
 
-use source::Source;
-
 pub fn parse<'a>(source: &'a str) -> Result<ast::Root<'a>> {
-  parse_source(Source::new(source))
+  ast::Root::new(source).parse()
 }
 
 pub fn parse_file<'a>(source: &'a std::path::Path) -> Result<ast::Root<'a>> {
-  parse_source(try!(Source::from_file(source)))
-}
-
-// TODO: Check if maybe useable outside of this
-fn parse_source<'a>(source: Source<'a>) -> Result<ast::Root<'a>> {
-  let root = ast::Root::new(source);
-  try!(root.parse());
-  Ok(root)
+  try!(ast::Root::from_file(source)).parse()
 }
